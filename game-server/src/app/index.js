57b9c236169666cccc7f13750ceea479e05e1e24
gabo4dev <gabriel4dev@gmail.com>
2018-03-10 23:16:43 -0300

    edit/update-index.js
    fixed several bugs on server-side, moveing, connecting, and firing.

--- game-server/src/app/index.js
+++ game-server/src/app/index.js
@@ -3,7 +3,6 @@ var server = require("http").Server(app);
 var io = require("socket.io")(server);
 
 server.listen(3000);
-//Global Variables -> Improve this later
 
 var enemies = [];
 var playerSpawnPoints= [];
@@ -19,23 +18,21 @@ io.on("connection", function(socket){
     currentPlayer.name = "unknown";
 
     socket.on("player_connect", function(){
-        console.log(currentPlayer.name + " recived: player_connect");
+        console.log("# " + currentPlayer.name + " recived: player_connect");
         for(var i = 0; i < clients.length; i++){
             var playerConected = {
                 name: clients[i].name,
                 position: clients[i].position,
-                rotation: clientes[i].rotation,
-                health: clientes[i].health
+                rotation: clients[i].rotation,
+                health: clients[i].health
             };
-            //in your current game we need to teel you about the other players
-            socket.emit("other_player_conected", playerConected);
-            console.log(currentPlayer.name + " emit: other_player_conected" + JSON.stringify(playerConected));
+            socket.emit("other_player_connected", playerConected);
+            console.log("# " + currentPlayer.name + " emit: other_player_conected" + JSON.stringify(playerConected));
         }
     });
 
     socket.on("play", function(data){
-        console.log(currentPlayer.name+" recived: play: "+ JSON.stringify(data));
-        //if this is the first person to join game, init the enemies
+        console.log("# " + currentPlayer.name+" recived: play: "+ JSON.stringify(data));
         if(clients.length === 0){
             numberOfEnemies = data.enemySpawnPoints.length;
             enemies = [];
@@ -60,8 +57,7 @@ io.on("connection", function(socket){
         var enemiesResponse = {
             enemies: enemies
         };
-        //we always will send the enemies when player join
-        console.log(currentPlayer.name+" emit: enemies: " + JSON.stringify(enemiesResponse));
+        console.log("# " + currentPlayer.name+" emit: enemies: " + JSON.stringify(enemiesResponse));
         socket.emit("enemies", enemiesResponse);
         var randomSpawnPoint = playerSpawnPoints[Math.floor(Math.random() * playerSpawnPoints.length)];
         currentPlayer = {
@@ -71,38 +67,35 @@ io.on("connection", function(socket){
             health: 100
         };
         clients.push(currentPlayer);
-        //in your current game, tell you that you have joined
-        console.log(currentPlayer.name+" emit: play: " +JSON.stringify(currentPlayer));
+        console.log("# " + currentPlayer.name+" emit: play: " +JSON.stringify(currentPlayer));
         socket.emit("play", currentPlayer);
-        //in your current game, we need to tell the other players about you.
-        socket.broadcast.emit("other_player_conected", currentPlayer);
+        socket.broadcast.emit("other_player_connected", currentPlayer);
     });
 
     socket.on("player_move", function(data){
-        console.log("received: player_move " + JSON.stringify(data));
+        console.log("@ " + "received: player_move " + JSON.stringify(data));
         currentPlayer.position = data.position;
         socket.broadcast.emit("player_move", currentPlayer);
     });
 
     socket.on("player_turn", function(data){
-        console.log("received: player_turn " + JSON.stringify(data));
+        console.log("@ " + "received: player_turn " + JSON.stringify(data));
         currentPlayer.rotation = data.rotation;
         socket.broadcast.emit("player_turn", currentPlayer);
     });
 
     socket.on("player_shoot", function(){
-        console.log(currentPlayer.name+" recv: player_shoot");
+        console.log("# " + currentPlayer.name +" recv: player_shoot");
         var data = {
             name: currentPlayer.name
         };
-        console.log(currentPlayer.name + "broadcasting: player_shoot: " + JSON.stringify(data));
+        console.log("# " + currentPlayer.name + " broadcasting: player_shoot: " + JSON.stringify(data));
         socket.emit("player_shoot", data);
         socket.broadcast.emit("player_shoot", data);
     });
 
     socket.on("health", function(data){
-        console.log(currentPlayer.name + "receive: health: " + JSON.stringify(data));
-        //only change the health once, we can do this by checking the originating player
+        console.log("@ " + currentPlayer.name + "received: health: " + JSON.stringify(data));
         if(data.from === currentPlayer.name){
             if(!data.isEnemy){
                 clients = clients.map(function(client, index){
@@ -126,16 +119,16 @@ io.on("connection", function(socket){
                 name: (!data.isEnemy) ? clients[indexDamaged].name : enemies[indexDamaged].name,
                 health: (!data.isEnemy) ? clients[indexDamaged].health : enemies[indexDamaged].health
             };
-            console.log(currentPlayer.name+ "broadcasting: health: " + JSON.stringify(response));
+            console.log("# " + currentPlayer.name+ "broadcasting: health: " + JSON.stringify(response));
             socket.emit("health", response);
             socket.broadcast.emit("health", response);
         }
     })
 
     socket.on("disconnect", function(){
-        console.log(currentPlayer.name+" received: disconnect " +currentPlayer.name);
+        console.log("@ " + currentPlayer.name+" received: disconnect " +currentPlayer.name);
         socket.broadcast.emit("other_player_disconnected", currentPlayer);
-        console.log(currentPlayer.name+ " broadcasting: other_player_disconnected: "+ JSON.stringify(currentPlayer));
+        console.log("# " + currentPlayer.name + " broadcasting: other_player_disconnected: " + JSON.stringify(currentPlayer));
         for(var i=0; i<clients.length;i++){
             if(clients[i].name === currentPlayer.name){
                 clients.splice(i,1);
@@ -144,7 +137,7 @@ io.on("connection", function(socket){
     });
 });
 
-console.log("=== Server running ... ===");
+console.log("=== Server running ...");
 
 function guid(){
     function s4(){
